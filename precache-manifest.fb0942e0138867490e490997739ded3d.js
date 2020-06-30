298828c1aae17e6957da70b0056e87f85c674a0a
RobertLudwick <rludwick@drew.edu>
2020-06-30 14:13:53 -0400

    Updates

---
+++ precache-manifest.fb0942e0138867490e490997739ded3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586782f60474c0f5cc1f276d2986a639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0795df36895e758b8def",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cbca804c7ad5c662df60",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0795df36895e758b8def",
+    "url": "/static/js/2.a1071373.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a1071373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbca804c7ad5c662df60",
+    "url": "/static/js/main.711242bd.chunk.js"
+  },
+  {
+    "revision": "f822f077cb3f82818b4c",
+    "url": "/static/js/runtime-main.d1f84a6d.js"
+  }
+]);
