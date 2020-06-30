ea062a9b39dd25ae14730df13294a3a261242150
RobertLudwick <rludwick@drew.edu>
2020-06-30 18:03:37 -0400

    added a resume download link

--- src/components/navi.jsx
+++ src/components/navi.jsx
@@ -11,13 +11,13 @@ class Navi extends React.Component {
   render () {
     return (
     <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">Robert Ludwick</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <Nav.Link href="https://drive.google.com/uc?export=download&id=18J0sZfvwXfM5uwHfaOoootrs-mYJP-6v">Resume</Nav.Link>
+        <NavDropdown title="Projects" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
