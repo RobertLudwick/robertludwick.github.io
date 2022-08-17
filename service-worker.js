39889e4848a421b7635d142e5eed4d48773d1c32
RobertLudwick <rludwick@drew.edu>
2022-08-17 17:48:27 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f75bdc14751f68462a1230a065fd8bbf.js"
+  "/precache-manifest.fca2293f32a404f3e29f057fe3419a7f.js"
 );
 
 self.addEventListener('message', (event) => {
